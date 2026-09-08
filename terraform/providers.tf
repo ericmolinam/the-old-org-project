2952ce1d5ca498ab52dec31164c0f51034baa949
ericmolinam <srmolina5100@gmail.com>
2026-09-08 19:55:20 +0200

    feat: add initial terraform configuration files

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+terraform {
+  required_version = ">= 1.15"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
