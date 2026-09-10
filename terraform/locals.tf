0fd4a09ac72084437c5c0879c345c572ada23252
ericmolinam <srmolina5100@gmail.com>
2026-09-10 18:50:20 +0200

    feat: add locals configuration for region and VPC CIDR

---
+++ terraform/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  region   = "eu-west-2"
+  vpc_cidr = "10.0.0.0/16"
+  env      = "dev"
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = local.vpc_cidr
 
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "dev-vpc"
+    Name = "${local.env}-vpc"
   }
 }
