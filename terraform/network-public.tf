3702592639b98ce1ac75cb10412140cc8f3f5b42
ericmolinam <srmolina5100@gmail.com>
2026-09-12 18:14:25 +0200

    feat: ensure public subnets automatically assign public IPs on launch

--- terraform/network-public.tf
+++ terraform/network-public.tf
@@ -12,6 +12,7 @@ resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = each.value.cidr_block
   availability_zone = each.value.availability_zone
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "${local.env}-public-${each.value.availability_zone}"
