a8f23e456ba8904bc3756109d3cddacdd41dc769
ericmolinam <srmolina5100@gmail.com>
2026-09-12 17:42:19 +0200

    feat: add ingress rules for public security group and implement dynamic ingress configuration

--- terraform/network-public.tf
+++ terraform/network-public.tf
@@ -37,3 +37,24 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[each.key].id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_security_group" "public" {
+  name        = "${local.env}-public"
+  vpc_id      = aws_vpc.this.id
+
+dynamic "ingress" {
+  for_each = local.ingress_rules
+
+  content {
+    description = ingress.value.description
+    from_port   = ingress.value.from_port
+    to_port     = ingress.value.to_port
+    protocol    = ingress.value.protocol
+    cidr_blocks = ingress.value.cidr_blocks
+  }
+}
+  tags = {
+    Name = "${local.env}-public"
+  }
+}
+
